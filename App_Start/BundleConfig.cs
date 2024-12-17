dea9397b7dc58cc22fa643709c5adef496a848ec
Benito <39175198+benitopatino@users.noreply.github.com>
2024-12-16 23:11:54 -0800

    add sort / search

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace EmployeeDirectory
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                                  "~/Scripts/Datatables/jquery.datatables.js",
+                                "~/Scripts/datatables/datatables.bootstrap4.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +26,8 @@ namespace EmployeeDirectory
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/datatables/css/datatables.bootstrap4.css"));
         }
     }
 }
---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeDirectory.DataLayer;
+
+namespace EmployeeDirectory.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeeDao _employeeDao;
+
+        public EmployeesController()
+        {
+            _employeeDao = new EmployeeDao();
+        }
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var empList = _employeeDao.GetAllEmployees();
+            //var empList = Enumerable.Empty<Employee>();
+            return View(empList);
+        }
+
+
+        public ActionResult Details(int? id)
+        {
+            var employee = _employeeDao.GetAllEmployees()
+                .SingleOrDefault(e => e.Id == id);
+
+            if (employee == null || id == 0)
+                return new HttpNotFoundResult();
+
+            return View(employee);
+        }
+    }
+}
