dfabe5cce56b4d6ba0d1a1961f33b9d65492c27f
Benito <39175198+benitopatino@users.noreply.github.com>
2025-03-15 20:29:18 -0700

    Add basic feaures

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EmployeeDirectory.DataLayer;
+using EmployeeDirectory.ViewModels;
 
 namespace EmployeeDirectory.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly EmployeeDao _employeeDao;
+        public EmployeeController()
+        {
+            _employeeDao = new EmployeeDao();
+        }
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
 
+            var empList = _employeeDao.GetAllEmployees()
+                .Where(e => e.Employed)
+                .ToList();
+
+
+            List<EmpViewModel> viewModel = new List<EmpViewModel>();
+            foreach (var e in empList)
+                viewModel.Add(new EmpViewModel(e.Name,e.DateHired.ToString("MM/dd/yyyy"), e.Id));
+
+
+            return View(viewModel);
+
+        }
+
+        [Route("employee/{id:int}")]
+        public ActionResult Details(int id)
+        {
+            var emp = _employeeDao.GetAllEmployees().FirstOrDefault(e => e.Id == id);
+            var model = new EmpViewModel(emp.Name, emp.DateHired.ToString("MM/dd/yyyy"), emp.Id);
+            return View(model);
         }
     }
 }
---
+++ ViewModels/EmpListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeDirectory.ViewModels
+{
+    public class EmpViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string HiredDate { get; set; }
+
+        public EmpViewModel(string name, string hiredDate, int id)
+        {
+            this.Name = name;
+            this.HiredDate = hiredDate;
+            this.Id = id;
+        }
+    }
+}
